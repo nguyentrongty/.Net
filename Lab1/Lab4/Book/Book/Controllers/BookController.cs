e5a1133d195e7f2f63e387d137704bf1f5144469
nguyentrongty <62439357+nguyentrongty@users.noreply.github.com>
2020-11-11 23:08:36 +0700

    first commit

---
+++ Lab1/Lab4/Book/Book/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Book.Models;
+
+
+namespace Book.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Get: Books/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST: Books/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(
+            [Bind("ID,Title,ReleaseDate,Cost")] BookModel bookModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(bookModel);
+        }
+    }
+}
